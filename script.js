29b7932faedbd6493b478780fd96c41986fb8abe
Minh Giang <minhgiang241@gmail.com>
2024-11-10 16:57:59 +0700

    ball with friction

--- script.js
+++ script.js
@@ -1,49 +1,169 @@
+"use strict";
 let canvas = document.querySelector("#canvas");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 var c = canvas.getContext("2d");
-/**
-c.fillStyle = "#a23459";
-c.fillRect(100, 100, 40, 40);
-c.beginPath();
-c.moveTo(50, 10);
-c.lineTo(300, 100);
-c.strokeStyle = "#fa34a3";
-c.stroke();
-c.lineTo(400, 10);
-c.strokeStyle = "#fa3";
-c.stroke();
-*/
+var mouse = {
+  x: 0,
+  y: 0,
+};
+addEventListener("mousemove", function (e) {
+  mouse.x = e.offsetX;
+  mouse.y = e.offsetY;
+});
+
+addEventListener("resize", function (e) {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+});
 
 var x = 200;
 var y = 200;
 var dx = 8;
 var dy = 8;
 var radius = 100;
+var maxRadius = 200;
+var minRadius = 5;
+var colorArray = [
+  "#ffaa33",
+  "#99ffaa",
+  "#00ff00",
+  "#4411aa",
+  "#ff1100",
+  "#1E90FF",
+  "#DDA0DD",
+  "#FF7F50",
+  "#FFD700",
+];
+var gravity = 1;
+var friction = 0.9;
 
-c.beginPath();
-c.arc(400, 400, 10, 0, Math.PI * 2, false);
-c.strokeStyle = "blue";
+function Circle(x, y, dx, dy, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.color = color;
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = this.color;
+    c.fillStyle = this.color;
+    c.lineWidth = 2;
+    c.fill();
+    c.stroke();
+  };
+
+  this.move = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+  };
+
+  this.freeMove = function () {
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > minRadius) {
+      this.radius -= 1;
+    }
+    this.move();
+    this.draw();
+  };
+}
+
+function Ball(x, y, dx, dy, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.color = color;
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = this.color;
+    c.fillStyle = this.color;
+    c.lineWidth = 2;
+    c.fill();
+    c.stroke();
+  };
+  this.update = function () {
+    if (this.y + this.radius + dy > canvas.height) {
+      this.dy = -this.dy * friction;
+    } else {
+      this.dy += gravity;
+    }
+    this.y += this.dy;
+    this.draw();
+  };
+
+  this.click = function () {
+    this.dy = randomIntFromRange(0, 100);
+  };
+}
+addEventListener("click", (e) => {
+  console.log("click");
+  for (let b of ballArr) {
+    b.click();
+  }
+});
+
+var circleArr = Array.from({ length: 2000 }, (_d) => {
+  return new Circle(
+    Math.random() * (innerWidth - minRadius * 2) + minRadius,
+    Math.random() * (innerHeight - minRadius * 2) + minRadius,
+    Math.random() * 8 - 4,
+    Math.random() * 8 - 4,
+    minRadius,
+    colorArray[Math.floor(Math.random() * colorArray.length)],
+  );
+});
+
+function randomIntFromRange(start, end) {
+  return Math.floor(Math.random() * (end - start) + start);
+}
+
+var ballArr = Array.from(
+  { length: 100 },
+  (_d) =>
+    new Ball(
+      randomIntFromRange(0, canvas.width),
+      innerHeight - radius,
+      dx,
+      dy,
+      radius,
+      // colorArray[Math.floor(Math.random() * colorArray.length)],
+      `rgb(
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)},
+        ${Math.floor(Math.random() * 256)}
+      )`,
+    ),
+);
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  c.beginPath();
-  c.arc(x, y, radius, Math.PI * 2, false);
-  c.fill();
-  c.fillStyle = "red";
-  c.strokeStyle = "red";
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (let ball of ballArr) {
+    ball.update();
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
 }
+
 animate();
