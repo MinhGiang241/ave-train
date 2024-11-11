364d5a1c7aae8b4e1b3ef8fa22e82ba00d96db2c
Nash Truong <Nash.Truong@avepoint.com>
2024-11-11 11:45:12 +0700

    scss library

--- typescript/src/sandbox.ts
+++ typescript/src/sandbox.ts
@@ -1,2 +1,26 @@
 "use strict";
-console.log("hello");
+function sum(array: number[]): number {
+  return array.reduce((a, b) => a + b);
+}
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+
+class Person {
+  constructor(name: string, age: number, gender: Gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  name: string;
+  age: number;
+  gender: Gender;
+
+  introduce() {
+    return `Hi, my name is ${this.name}, I'm ${this.age}, my gender is ${this.gender}`;
+  }
+}
